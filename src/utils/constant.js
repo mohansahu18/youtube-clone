f554bbe27eaa41c599bc8f330550386fd3d1ad73
mohan lal sahu <123477420+mohansahu18@users.noreply.github.com>
2023-09-02 14:03:15 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,7 @@ const MY_GOOGLE_API_KEY = 'AIzaSyDrtCrNUk9akMhSrY0WzJy8v7pCSDryFdM'
 export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=' + MY_GOOGLE_API_KEY
 
 
-export const YOUTUBE_SEARCH_SUGGESTION_API = 'https://cors-anywhere.herokuapp.com/https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+export const YOUTUBE_SEARCH_SUGGESTION_API = 'https://corsproxy.io/?/https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 
 
 export const YOUTUBE_SEARCH_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=50&key= + ${MY_GOOGLE_API_KEY}&q=`
